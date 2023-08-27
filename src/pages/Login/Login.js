fb1a54848c0acbe721db6f35be75432ef6949ed4
Anus Ali Khan <anusalikhan1405@gmail.com>
2023-08-27 23:15:07 +0500

    Login page in progress

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,17 +1,24 @@
 import React from "react";
 
 const Login = () => {
-  return <div className="login-page">
-    <h1 className="flex items-center justify-center ">Login</h1>
-    <section className="flex flex-col justify-center items-center h-10">
-      <form className="login-container flex flex-col items-center">
-        <input type="text" placeholder="youremail@gmail.com"/>
-        <input type="password" placeholder="password"/>
-        <button type="submit">Login</button>
-        <p>Don't have an account? Sign Up</p>
-      </form>
-    </section>
-  </div>
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <div className="w-fit p-5 flex flex-col items-center gap-6 justify-between border border-solid border-gray-950">
+        <h1 className="">Login</h1>
+        <form className="login-container flex gap-5 flex-col items-center">
+          <input type="text" placeholder="youremail@gmail.com" />
+          <input type="password" placeholder="password" />
+          <button
+            className="border border-solid bg-amber-500 font-semibold text-white p-2 rounded-xl w-full"
+            type="submit"
+          >
+            Login
+          </button>
+          <p>Don't have an account? Sign Up</p>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
