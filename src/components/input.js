fe6b11f9e6d21895b1f6583fe59eb4161bd5906e
Anus Ali Khan <anusalikhan1405@gmail.com>
2023-08-28 20:34:25 +0500

    Changing UI of Login page

---
+++ src/components/input.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Input = () => {
+  return (
+    <div className="input-container flex flex-col ">
+      <input type="text" placeholder="email@gmail.com" />
+      <input type="password" placeholder="password" />
+    </div>
+  );
+};
+
+export default Input;
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,20 +1,38 @@
 import React from "react";
+import Input from "../../components/input";
 
 const Login = () => {
   return (
     <div className="h-screen flex justify-center items-center">
       <div className="w-fit p-5 flex flex-col items-center gap-6 justify-between border border-solid border-gray-950">
-        <h1 className="">Login</h1>
         <form className="login-container flex gap-5 flex-col items-center">
-          <input type="text" placeholder="youremail@gmail.com" />
-          <input type="password" placeholder="password" />
-          <button
-            className="border border-solid bg-amber-500 font-semibold text-white p-2 rounded-xl w-full"
-            type="submit"
-          >
-            Login
-          </button>
-          <p>Don't have an account? Sign Up</p>
+          <h1 className="">Welcome Back :)</h1>
+          <p>
+            To keep login with us please connected with your personal
+            information by email address and password
+            <span>
+              <img src="/src/assets/bell_1156949.png" alt="bell icon" />
+            </span>
+          </p>
+          <Input />
+          <div className="flex items-end">
+            <input type="checkbox" />
+            <p>Remember me</p>
+            <div>
+              <p>Forgot Password?</p>
+            </div>
+          </div>
+          <div className="btns flex gap-5 w-1/2">
+            <button
+              className="login-btn border border-solid bg-sky-500 font-semibold text-white p-2 rounded-full w-full"
+              type="submit"
+            >
+              Login Now
+            </button>
+            <button type="submit" className="signup-button rounded-full w-full">
+              Create Account
+            </button>
+          </div>
         </form>
       </div>
     </div>
