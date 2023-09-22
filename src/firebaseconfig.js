51f0a255edd84e0ce41e5f466220c652e1778cc3
Anus Ali Khan <anusalikhan1405@gmail.com>
2023-09-22 20:26:39 +0500

    setup Firebase

---
+++ src/firebaseconfig.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCfaAbPJGt4z93YzYshXFxXkQDuNemRlDU",
+  authDomain: "bidverse-web.firebaseapp.com",
+  projectId: "bidverse-web",
+  storageBucket: "bidverse-web.appspot.com",
+  messagingSenderId: "80809214123",
+  appId: "1:80809214123:web:0c2d66868991f6669937ee",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
