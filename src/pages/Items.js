7df66309ca30d84472085270dda2693026635dbc
Anus Ali Khan <anusalikhan1405@gmail.com>
2023-09-29 22:06:49 +0500

    favourite icon added

--- src/pages/Items.js
+++ src/pages/Items.js
@@ -1,15 +1,17 @@
 import React from "react";
+import { AiOutlineHeart } from "react-icons/ai";
 
-export default function Items({ id, Title, Description, Price, productImage }) {
+export default function Items({ data }) {
   return (
     <div className="border border-solid rounded-2xl p-6  w-1/4 h-auto text-xl bg-white">
-      <img src={productImage} />
+      <AiOutlineHeart className="mb-2 h-10 w-6" />
+      <img src={data.productImage} />
       <div className="mt-4 text-center">
         <p className="font-semibold">
-          <b>{Title}</b>
+          <b>{data.Title}</b>
         </p>
-        <p>{Description}</p>
-        <p className="font-semibold">Rs.{Price}</p>
+        <p>{data.Description}</p>
+        <p className="font-semibold">Rs.{data.Price}</p>
       </div>
     </div>
   );
