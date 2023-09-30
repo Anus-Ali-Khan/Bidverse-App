5507f5abde4da75bd6d09b986295dbbe80f04b19
Anus Ali Khan <anusalikhan1405@gmail.com>
2023-09-30 23:05:25 +0500

    working on my profile screen

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { LiaUserCircle } from "react-icons/lia";
 
 export default function MyProfile() {
   return (
-    <div className="bg-slate-200 h-screen flex justify-center items-center">
-      <div className="h-[80%] w-[70%] rounded-[2.5rem] bg-blue-500  m-auto ">
-        <div className="h-[100%] w-[30%] bg-blue-700 rounded-s-[2.5rem]"></div>
+    <div className=" h-screen flex justify-center items-center p-12">
+      <div className="h-[80%] w-[20%]  flex justify-center bg-orange-400">
+        <LiaUserCircle className="h-56 w-56 mt-8" />
+      </div>
+      <div className="h-[80%] w-[80%]  m-auto bg-blue-500 flex flex-col justify-center items-center">
+        <label>User Name</label>
+        <input type="text" placeholder="Anus Ali Khan" />
+
+        <label htmlFor="">Phone Number</label>
+        <input type="number" placeholder="03110162290" />
+        <button>Edit Profile</button>
+        <button>Your Products</button>
+        <button>Your Bidded products</button>
+        <button>Sign out</button>
       </div>
     </div>
   );
