b995c770f7321a35849c56a6a83bb6084c94bc84
Anus Ali Khan <anusalikhan1405@gmail.com>
2023-12-13 12:53:04 +0500

    working on stripe

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -1,10 +1,21 @@
-import { PaymentElement } from "@stripe/react-stripe-js";
+import React, { useState } from "react";
+import "../components/Payment.css";
 
 const CheckoutForm = () => {
+  const [message, setMessage] = useState(null);
+  const [isProcessing, setIsProcessing] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <form>
-      <PaymentElement />
-      <button>Submit</button>
+    <form id="payment-form" onSubmit={handleSubmit}>
+      <button id="payment-button" disabled={isProcessing}>
+        <span>{isProcessing ? "Processing ..." : "Pay now"}</span>
+      </button>
+      {/* Show any error or success messages */}
+      {message && <div id="payment-message">{message}</div>}
     </form>
   );
 };
