c32fa6798bb3035390a63161f2c1767b22c166d6
Anus Ali Khan <anusalikhan1405@gmail.com>
2023-12-18 20:36:53 +0500

    working on paypal

--- src/components/PayPal.js
+++ src/components/PayPal.js
@@ -1,18 +1,50 @@
-import React from "react";
 import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 
-const PayPal = () => {
-  const initialOptions = {
-    clientId: "test",
-    currency: "USD",
-    intent: "capture",
-  };
+export default function App() {
+    function createOrder() {
+        return fetch("/my-server/create-paypal-order", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            // use the "body" param to optionally pass additional order information
+            // like product ids and quantities
+            body: JSON.stringify({
+                cart: [
+                    {
+                        id: "YOUR_PRODUCT_ID",
+                        quantity: "YOUR_PRODUCT_QUANTITY",
+                    },
+                ],
+            }),
+        })
+            .then((response) => response.json())
+            .then((order) => order.id);
+    }
+    function onApprove(data) {
+          return fetch("/my-server/capture-paypal-order", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              orderID: data.orderID
+            })
+          })
+          .then((response) => response.json())
+          .then((orderData) => {
+                const name = orderData.payer.name.given_name;
+                alert(`Transaction completed by ${name}`);
+          });
 
-  return (
-    <PayPalScriptProvider deferLoading={true} options={initialOptions}>
-      <PayPalButtons style={{ layout: "horizontal" }} />
-    </PayPalScriptProvider>
-  );
-};
-
-export default PayPal;
+        }
+    }
+    return (
+        <PayPalScriptProvider options={{ clientId: "test" }}>
+            <PayPalButtons
+                createOrder={createOrder}
+                onApprove={onApprove}
+            />
+        </PayPalScriptProvider>
+    );
+}
